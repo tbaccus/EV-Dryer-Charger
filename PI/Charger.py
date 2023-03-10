4489d3b3395b6b69b215ce4c3013827890fce460
Daniel Guerra-Rojas <82733805+dguerr45@users.noreply.github.com>
2023-03-10 13:18:26 -0800

    Typos and Suggestions/Questions
    There were typos with some of the variables like "SIDE" in statements. Added questions in the form of comments so I wouldn't forget to ask them in the future.

--- PI/Charger.py
+++ PI/Charger.py
@@ -58,24 +58,26 @@ STATE = EV_State.UNKNOWN
 
 def init_charger():
     if(SIDE is Charge_Side.CAR_SIDE):
-    	print("Initializing car chargin peripherals...")
+    	print("Initializing car charging peripherals...")
     	GPIO.output(PILOT_PIN, True)
     elif(SIDE is Charge_Side.DRYER_SIDE):
     	print("Initializing dryer side peripherals...")
+        ### don't we need to make PILOT_PIN False in this case? ###
+        #GPIO.output(PILOT_PIN, False)
 
 def enable_relay(side):
     GPIO.output(ENABLE_CAR_PIN, False)
     GPIO.output(ENABLE_DRYER_PIN, False)
     sleep(1)
-    if(side is Charge_Side.CAR_SIDE):
+    if(SIDE is Charge_Side.CAR_SIDE):
     	print("Car side relay enabled.")
     	GPIO.output(ENABLE_CAR_PIN, True)
     	GPIO.output(ENABLE_DRYER_PIN, False)
-    elif(side is Charge_Side.DRYER_SIDE):
+    elif(SIDE is Charge_Side.DRYER_SIDE):
     	print("Dryer side relay enabled.")
     	GPIO.output(ENABLE_CAR_PIN, False)
     	GPIO.output(ENABLE_DRYER_PIN, True)
-    elif(side is Charge_Side.NEITHER):
+    elif(SIDE is Charge_Side.NEITHER):
     	print("Both sides disabled.")
     	GPIO.output(ENABLE_CAR_PIN, False)
     	GPIO.output(ENABLE_DRYER_PIN, False)
@@ -95,11 +97,16 @@ def read_pilot_state():
     for state in EV_State:
     	diff.append(abs(Vpwm - int(state)))
 
+    ### why min(diff)? Is there a chance we subtract a smaller from a larger number? ###
+    ### example: min(diff) = 3 - 6 = -3 ###
     curr_state = states[diff.index(min(diff))]
 
     return curr_state
 
 def test_side_enabled():
+    ### does "return [case 1] if... else [case 2] if... else [case 3]" even work? ###
+    ### I'm not sure if it works if there's two else cases ###
+    ### Also, is this for safety checks on boot? or part of the constant checking in the future multi-threading? ###
     return Charge_Side.CAR_SIDE if GPIO.input(RELAY1_TEST) else Charge_Side.DRYER_SIDE if GPIO.input(RELAY2_TEST) else Charge_Side.NEITHER
 
 def exit():
