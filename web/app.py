ee0557d2441abd457d88d9e33b6f65fdaad2adaa
Tanner Baccus <tbaccus@uci.edu>
2023-03-09 22:36:22 -0800

    Cleanup

--- web/app.py
+++ web/app.py
@@ -22,21 +22,12 @@ app = Flask(__name__)
 # gpio.setup(27, gpio.OUT)
 
 
-# @app.before_request
-# def before_request():
-#     if not request.is_secure:
-#         url = request.url.replace('http://', 'https://', 1)
-#         code = 301
-#         return redirect(url, code=code)
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     currOut = 1
     if request.method == 'POST':
         form = request.form
         currOut = currentOutput(form)
-        # lightOn()
 
     return render_template('index.html', currOut=currOut)
 
